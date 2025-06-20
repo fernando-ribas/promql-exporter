35cfafb6b0ccc896da9260391b3f36b9d37d130a
fernando-ribas <fernando.ribas@dynatrace.com>
2025-06-20 18:11:10 -0300

    Update exporter.go

--- exporter/exporter.go
+++ exporter/exporter.go
@@ -1,16 +1,55 @@
 package exporter
 
-type Metrics struct {
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type QueryResponse struct {
 	Status string `json:"status"`
 	Data   struct {
 		ResultType string `json:"resultType"`
 		Result     []struct {
 			Metric map[string]string `json:"metric"`
-			Value  []any             `json:"value"`
+			Value  [2]interface{}    `json:"value"`
 		} `json:"result"`
 	} `json:"data"`
 }
 
-func GetMetrics(endpoint string, headers map[string]string) (*Metrics, error) {
-	return HTTPRequest[*Metrics](endpoint, `/api/v1/query?query={__name__!=""}`, headers)
+func GetMetricsWithQuery(endpoint string, headers map[string]string, rule string) (*QueryResponse, error) {
+	url := fmt.Sprintf("%s/api/v1/query?query=%s", endpoint, rule)
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	for k, v := range headers {
+		req.Header.Set(k, v)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("received non-200 response code: %d", resp.StatusCode)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var queryResp QueryResponse
+	err = json.Unmarshal(body, &queryResp)
+	if err != nil {
+		return nil, err
+	}
+
+	return &queryResp, nil
 }
